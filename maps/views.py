e0c6a6719101afc1939a0bc26b3af06e305c87e1
msmsh83 <msmsh83@hotmail.com>
2014-01-05 00:33:35 +0200

    Employers Activity Statistics Fix

--- maps/views.py
+++ maps/views.py
@@ -337,7 +337,7 @@ def jobs_employers_statistics_getdata(request):
         
         header_sql = ("select datejoined,max(jobs_per_employer),min(jobs_per_employer), avg(jobs_per_employer), median(jobs_per_employer)")
         
-        from_sql = ("from (select count(cj.id) as jobs_per_employer, u.id,substring(to_char(au.date_joined,'YYYY-MM-DD HH24:MI:SS'),1,"+grouper+") as datejoined from contracts_job cj inner join users u on u.id=cj.employer_id inner join auth_user au on u.django_user_id=au.id where au.date_joined>='"+t1+"' and au.date_joined<='"+t2+"' group by u.id,au.date_joined order by jobs_per_employer desc) total group by datejoined order by datejoined desc;")
+        from_sql = ("from (select count(cj.id) as jobs_per_employer, u.id,substring(to_char(au.date_joined,'YYYY-MM-DD HH24:MI:SS'),1,"+grouper+") as datejoined from contracts_job cj inner join users u on u.id=cj.employer_id inner join auth_user au on u.django_user_id=au.id where au.date_joined>='"+t1+"' and au.date_joined<='"+t2+"' group by u.id,au.date_joined order by jobs_per_employer desc) total group by datejoined order by datejoined;")
         sql = (header_sql + from_sql)
         
         #print freelancersql
