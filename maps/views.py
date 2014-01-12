f24bfe9dc4c87c3232fccc4ceb61616400836bde
msmsh83 <msmsh83@hotmail.com>
2014-01-12 19:34:44 +0200

    Multiple Databases Usage

--- maps/views.py
+++ maps/views.py
@@ -16,6 +16,7 @@ import dateutil.parser
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Q
 from django.db import connection
+from django.db import connections
 from django.contrib.auth.decorators import login_required
 
 
@@ -32,145 +33,38 @@ def home(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
     #return HttpResponse(t.render(c))
 
-def userlist(request,day,month,year):
-    date1 = dateutil.parser.parse(month + '-' + day +'-' + year + ' 00:00:00 GMT')
-    user_list = AuthUser.objects.filter(date_joined__gt= date1)
-    t = loader.get_template('userlist.xml')
-    c = Context({
-        'user_list': user_list,
-    })
-#    return render_to_response('index.html', content)
-    return HttpResponse(t.render(c), mimetype='application/xml')
 
 
-def skillslist(request):
-    context = {'skills': SkillsSkill.objects.filter(published=True).values('id','name', 'slug').order_by('name')}
-    return HttpResponse(render_to_string('skills.json', context, context_instance=RequestContext(request)), mimetype='application/json')
+def customQuery(sql, db):
+    if db==0:
+        return customQueryOffline(sql)
+    else:
+        return customQueryLive(sql)
 
-def skillslistp(request,p):
-    context = {'skills': SkillsSkill.objects.filter(published=True, id__gt=p).values('id','name', 'slug').order_by('name')}
-    return HttpResponse(render_to_string('skills.json', context, context_instance=RequestContext(request)), mimetype='application/json')
-    
-    # data=SkillsSkill.objects.all().values_list('name', 'slug')
-    #return HttpResponse(simplejson.dumps(list(data)))
 
-@csrf_exempt
-def userlistparam(request):
-    if request.method == 'POST':
-        objs = simplejson.loads(request.raw_post_data)
-	q1 = Q(date_joined__gte = dateutil.parser.parse(objs['nab-joined-from']  + ' 00:00:00+00'))
-	q2 = Q(date_joined__lte = dateutil.parser.parse(objs['nab-joined-to'] + ' 23:59:59+00'))
-	
-        q3 = Q(last_login__gte = dateutil.parser.parse(objs['nab-login-from']  + ' 00:00:00+00'))
-        q4 = Q(last_login__lte = dateutil.parser.parse(objs['nab-login-to'] + ' 23:59:59+00'))
 
-        data = AuthUser.objects.filter(q1 & q2 & q3 & q4)
-        print request.raw_post_data
-        #objs = simplejson.loads(request.raw_post_data)
-        print dateutil.parser.parse(objs['nab-joined-from']  + ' 00:00:00+00')
-        print dateutil.parser.parse(objs['nab-joined-to'] + ' 23:59:59+00')
-	
-        t = loader.get_template('userlist.xml')
-        
-        c = Context({'user_list': data})
-        #print t.render(c)
-        results = customQuery("Select * from Users u inner join Auth_User au on u.django_user_id= au.id limit 10")
-        print results[1]
-        return HttpResponse(t.render(c), mimetype='application/xml')
-
-def customQuery(sql): 
-    cursor = connection.cursor()
+def customQueryOffline(sql):
+    cursor = connections['offline'].cursor()    
     cursor.execute(sql,[])
+    #transaction.commit_unless_managed(using='offline')
+    print sql
     result_list = [] 
     for row in cursor.fetchall(): 
         result_list.append(row) 
     return result_list 
 
-@csrf_exempt
-def markerlist(request):
-    if request.method == 'POST':
-        objs = simplejson.loads(request.raw_post_data)
+def customQueryLive(sql): 
+    cursor = connections['live'].cursor()
+    cursor.execute(sql,[])
+    #transaction.commit_unless_managed(using='live')
+    result_list = [] 
+    for row in cursor.fetchall(): 
+        result_list.append(row) 
+    return result_list 
 
-        genders = objs['nab-gender']	
-        if len(genders) == 0:
-            genders.append('2')
-           
-        nabskillslistsql = ""
-	nabskillslist = objs['nab-skills']
-        if len(nabskillslist) != 0:
-            nabskillslistsql = "and su.skill_id in (" + str(', '.join(nabskillslist)) + ")"
-
-        empskillslistsql = ""
-        empskillslist = objs['emp-skills']
-        if len(empskillslist) != 0:
-            empskillslistsql = "and sk.skill_id in (" + str(', '.join(empskillslist)) + ")"
-
-
-        t = loader.get_template('markers.xml')
-
-        nabsql = "select au.first_name,au.last_name,au.email,u.lat, u.longi, u.photo, u.homepage, 'Nabbesher' \
-           from users u inner join auth_user au on u.django_user_id=au.id inner join skills_users su on su.id_user = u.id \
-           where au.date_joined >= '" + objs['nab-joined-from'] +"' and au.date_joined <= '" +objs['nab-joined-to'] + "' and \
-           au.last_login >= '" + objs['nab-login-from'] +"' and au.last_login <='" +objs['nab-login-to'] + "' and \
-           u.gender in (" +str(', '.join(genders)) + ") and u.is_employer<>True " + nabskillslistsql 
-        empsql = "select au.first_name,au.last_name,au.email,u.lat, u.longi, u.photo, u.homepage, 'Employer' \
-           from users u inner join auth_user au on u.django_user_id=au.id inner join skills sk on sk.id_user = u.id \
-           where au.date_joined >= '" + objs['emp-joined-from'] +"' and au.date_joined <= '" +objs['emp-joined-to'] + "' and \
-           au.last_login >= '" + objs['emp-login-from'] +"' and au.last_login <='" +objs['emp-login-to'] + "' and \
-           u.gender in (" +str(', '.join(genders)) + ") and u.is_employer=True " + empskillslistsql
-        sql = nabsql + " union  " + empsql
-         
-        print sql
-        results = customQuery(sql)
-        #print sql
-	c = Context({'user_list': results})
-        #print t.render(c)
-        return HttpResponse(t.render(c), mimetype='application/xml')
-
-
-@csrf_exempt
-def applicationlist(request):
-    if request.method == 'POST':
-        objs = simplejson.loads(request.raw_post_data)
-
-	jaskillslistsql = ""
-        jaskillslist = objs['ja-skills']
-        if len(jaskillslist) != 0:
-            jaskillslistsql = "and sk.skill_id in (" + str(', '.join(jaskillslist)) + ")"
-
-        sql = "select mm.sent_at,mm.opportunity_id, mm.sender_id, u1.lat as f_lat,u1.longi as f_lng, mm.recipient_id, u2.lat as t_lat,u2.longi as t_lng, u1.photo as photo1, u1.homepage as homepage1, u2.photo as photo2, u2.homepage as homepage2,sk.description from messages_message mm inner join users u1 on mm.sender_id=u1.id inner join users u2 on mm.recipient_id=u2.id inner join skills sk  on sk.id = mm.opportunity_id inner join skills_skill ss on ss.id=sk.skill_id where mm.sent_at>='"+ objs['ja-from']+"' and mm.sent_at<='"+objs['ja-to']+"' " + jaskillslistsql
-        t = loader.get_template('lines.xml')
-        print sql
-        results = customQuery(sql)
-        #print results
-        c = Context({'line_list': results})
-        #print t.render(c)
-        return HttpResponse(t.render(c), mimetype='application/xml')
-
-
-@csrf_exempt
-def skillsstatistics(request):
-    if request.method == 'POST':
-        objs = simplejson.loads(request.raw_post_data)
-        #print objs['radius']
-        #R = objs['radius']
-	#print objs['lat']
-#        sql = "select ss.name,count(u.id) as cnt from skills_skill ss inner join skills_users su on ss.id=su.skill_id inner join users u on su.id_user=u.id where sqrt(pow(u.lat-"+ str(objs['lat'])+",2)*63101+pow(u.longi-"+ str(objs['lng'])+",2)*63101) <= " + str(objs['radius']/1000) + " group by ss.name order by cnt desc limit 10"
-        sql = "select Jobs.name As Skill,Skills.cnt as SkillCount, Jobs.cnt as JobCount, Skills.cnt/Jobs.cnt as Availability\
- from (select  ss.name,count(u.id) as cnt, 'Job' as Type from skills_skill ss inner join skills s on s.skill_id = ss.id inner join users u on u.id= s.id_user\
- where sqrt(pow(u.lat-"+ str(objs['lat'])+",2)*63101+pow(u.longi-"+ str(objs['lng'])+",2)*63101)<= " + str(objs['radius']/1000) + "  group by ss.name order\
- by cnt desc) Jobs inner join (select ss.name,count(u.id) as cnt,'Skill' as Type from skills_skill ss inner join skills_users\
- su on ss.id=su.skill_id inner join users u on su.id_user=u.id where sqrt(pow(u.lat-"+ str(objs['lat'])+",2)*63101+pow(u.longi-"+ str(objs['lng'])+",2)*63101)\
- <= " + str(objs['radius']/1000) + " group by ss.name order by cnt desc) Skills on Jobs.name= Skills.Name order by JobCount desc limit 20"
-        print sql
-        t = loader.get_template('skillsstatistics.xml')
-        results = customQuery(sql)
-        c = Context({'skills_list': results})
-        #print t.render(c)
-        return HttpResponse(t.render(c), mimetype='application/xml')
         
         
-        
+       
         
 def freelancerdemography_report(request):
     
@@ -193,9 +87,8 @@ def freelancerdemography_getdata(request):
 	select sum(usercount) as usercount, 'All The Rest' from (select count(*) as usercount,replace(reverse(substring(reverse(replace(formatted_address,'-',',')),\
 	1,position(',' in reverse(replace(formatted_address,'-',','))))),', ','') as country from users group by country order by usercount desc)\
 	 total where usercount<"+objs['limit']+" order by usercount desc;"
-        results = customQuery(sql)
-        
-        print sql
+        results = customQuery(sql,0)
+
         c = Context({'countries': results})
    
         return HttpResponse(render_to_string('freelancersdemography.json', c, context_instance=RequestContext(request)), mimetype='application/json')
@@ -216,7 +109,7 @@ def freelancersgender_getdata(request):
 
         sql = "select usercount, case gender when 0 then 'Male' when 1 then 'Female' end from \
          (select count(*) as usercount, gender from users where gender < 2 group by gender) total;"
-        results = customQuery(sql)
+        results = customQuery(sql,0)
         print results
  
         c = Context({'genders': results})
@@ -238,8 +131,8 @@ def freelancersages_getdata(request):
         #objs = simplejson.loads(request.raw_post_data)
 
         sql = "select  sum(ucount) as usercounts,case when ageu <18 then '1) Under 18' when ageu >= 18 and ageu<=24 then '2) 18 to 24' when ageu >= 25 and ageu<=34 then '3) 25 to 34' when ageu >= 35 then '4) Over 35' END as age_range from (select count(total.id) as ucount, 2013 - total.yobn as ageu from (select t1.id, t1.yob :: integer yobn from(select id, substring(dob,length(dob)-3, length(dob)) as yob from users where dob<>'') t1 where t1.yob ~E'^\\\d+$') total group by ageu order by ageu) total group by age_range order by age_range;"
-        results = customQuery(sql)
-        print sql
+        results = customQuery(sql,0)
+
         print results
  
         c = Context({'ages': results})
@@ -300,8 +193,7 @@ def dashboard_getdata(request):
         invoicepaid_sql = ("(select count(distinct cp.id) as invoicepaid_count,substring(to_char(cp.timestamp,'YYYY-MM-DD HH24:MI:SS'),1,"+grouper+") as datepaid from contracts_invoice ci inner join contracts_paidout cp on ci.paid_out_id=cp.id  where cp.timestamp>='"+t1+"' and cp.timestamp<='"+t2+"' group by datepaid) invoicespaid on invoicespaid.datepaid=contractsmessages.msgdate")
         sql = (header_sql + workflow_messages_sql + freelancers_sql + employers_sql + realemployers_sql + jobs_sql + contractsmessages_sql + porposals_sql + application_sql + invoicesent_sql + invoicepaid_sql + "  order by msgdate")
         
-        print sql
-        results = customQuery(sql)
+        results = customQuery(sql,0)
 
         #print results
  
@@ -342,8 +234,8 @@ def jobs_employers_statistics_getdata(request):
         from_sql = ("from (select count(cj.id) as jobs_per_employer, u.id,substring(to_char(au.date_joined,'YYYY-MM-DD HH24:MI:SS'),1,"+grouper+") as datejoined from contracts_job cj inner join users u on u.id=cj.employer_id inner join auth_user au on u.django_user_id=au.id where au.date_joined>='"+t1+"' and au.date_joined<='"+t2+"'  and cj.created_at<=(date_joined + INTERVAL '"+period+" Day') group by u.id,au.date_joined order by jobs_per_employer desc) total group by datejoined order by datejoined;")
         sql = (header_sql + from_sql)
         
-        print sql
-        results = customQuery(sql)
+
+        results = customQuery(sql,0)
 
         #print results
  
@@ -381,7 +273,7 @@ def jobs_applications_statistics_getdata(request):
         sql = ("select au.first_name || ' ' || au.last_name as employer_name,au.email as Employer_Email, cj.id as job_id,substring(cj.title,1,30) as job_title,substring(to_char(cj.created_at,'YYYY-MM-DD HH24:MI:SS'),1,16) as created_at,count(distinct ca.id) as application_count,count(distinct cp.message_ptr_id) as proposal_count, sum(case when cp.status=4 then 1 else 0 end) as acceptedproposal_count from contracts_job cj left outer join contracts_application ca   on cj.id=ca.job_id left outer join contracts_message cm on cm.application_id=ca.id left outer join contracts_proposal cp on cp.message_ptr_id=cm.id inner join users u on u.id=cj.employer_id inner join auth_user au on u.django_user_id=au.id where created_at>='"+t1+"' and created_at<='"+t2+"' "+searchsql+" group by employer_name,cj.id,job_title,cj.created_at,au.email order by cj.created_at desc;")
         
        
-        results = customQuery(sql)
+        results = customQuery(sql,1)
       
  
         c = Context({'statistics': results})
@@ -413,8 +305,7 @@ def sign_job_proposal_invoice_getdata(request):
            
         sql = ("select * from (select count(distinct au.email) as signed_up,count(distinct cj.id) as posted_jobs,sum(case when cp.status=4 then 1 else 0 end) as proposals_paid, count(distinct invoices.message_ptr_id) as invoices_paid from users u inner join auth_user au on u.django_user_id=au.id left outer join contracts_job cj  on cj.employer_id=u.id left outer join contracts_application ca on ca.job_id=cj.id left outer join contracts_message cm on cm.application_id=ca.id left outer join contracts_proposal cp on cp.message_ptr_id=cm.id left outer join (select distinct ca1.job_id,ci.status,ci.message_ptr_id,ca1.applicant_id from contracts_invoice ci inner join contracts_message cm1 on cm1.id=ci.message_ptr_id inner join contracts_application ca1 on ca1.id=cm1.application_id where ci.paid_out_id is not null) invoices on invoices.applicant_id=u.id) total;")
         
-        print sql
-        results = customQuery(sql)
+        results = customQuery(sql,0)
  	print results	
         c = Context({'statistics': results})
         return HttpResponse(render_to_string('sign_job_proposal_invoice_simple.json', c, context_instance=RequestContext(request)), mimetype='application/json')                   
@@ -436,8 +327,8 @@ def sign_job_proposal_invoice_simple_getdata(request):
            
         sql = ("select * from (select count(distinct au.email) as signed_up,count(distinct cj.id) as posted_jobs,sum(case when cp.status=4 then 1 else 0 end) as proposals_paid, count(distinct invoices.message_ptr_id) as invoices_paid from users u inner join auth_user au on u.django_user_id=au.id left outer join contracts_job cj  on cj.employer_id=u.id left outer join contracts_application ca on ca.job_id=cj.id left outer join contracts_message cm on cm.application_id=ca.id left outer join contracts_proposal cp on cp.message_ptr_id=cm.id left outer join (select distinct ca1.job_id,ci.status,ci.message_ptr_id,ca1.applicant_id from contracts_invoice ci inner join contracts_message cm1 on cm1.id=ci.message_ptr_id inner join contracts_application ca1 on ca1.id=cm1.application_id where ci.paid_out_id is not null) invoices on invoices.applicant_id=u.id) total;")
         
-        print sql
-        results = customQuery(sql)
+
+        results = customQuery(sql,0)
  	print results	
         c = Context({'statistics': results})
         return HttpResponse(render_to_string('sign_job_proposal_invoice.json', c, context_instance=RequestContext(request)), mimetype='application/json')                                         
